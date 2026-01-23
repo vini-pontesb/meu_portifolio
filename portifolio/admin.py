618f4e0a90cb05af1f44b9ee61b614c22fc0d404
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-01-23 00:07:27 -0300

    Adicionando dados aos models e finalizando a página inicial

--- portifolio/admin.py
+++ portifolio/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Contato, Profile, Project, Skill
 
-# Register your models here.
+
+class Projects(admin.ModelAdmin):
+    list_display = ['titulo', 'descricao_curta', 'data_criacao']
+    search_fields = ['titulo']
+
+
+class Profiles(admin.ModelAdmin):
+    list_display = ['nome', 'bio_curta']
+
+class Skills(admin.ModelAdmin):
+    list_display = ['skill', 'nivel', 'categoria']
+    search_fields = ['skill']
+    list_filter = ['categoria']
+
+
+admin.site.register(Contato)
+admin.site.register(Profile, Profiles)
+admin.site.register(Project, Projects)
+admin.site.register(Skill, Skills)
--- portifolio/views.py
+++ portifolio/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contato, Project, Profile, Skill
 
 def home (request):
     home = 'home'
-    context = {'home': home}
+    perfil = Profile.objects.all()
+    context = {'perfil': perfil, 'home': home}
     return render(request, 'home.html', context)
-
