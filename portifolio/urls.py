0f453bf4d420745b44d7af47bcc187b800fc4837
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-01-23 23:26:00 -0300

    Construindo a página sobre e a página projeto

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('sobre/<int:id_profile>', views.sobre, name='sobre'),
+    path('projetos/', views.list_projetos, name='projetos'),
 ]
--- portifolio/views.py
+++ portifolio/views.py
@@ -4,6 +4,15 @@ from .models import Contato, Project, Profile, Skill
 
 def home (request):
     home = 'home'
-    perfil = Profile.objects.all()
-    context = {'perfil': perfil, 'home': home}
+    context = {'home': home}
     return render(request, 'home.html', context)
+
+def sobre (resquest, id_profile):
+    perfil = Profile.objects.get(id = id_profile)
+    context = {'perfil': perfil}
+    return render(resquest, 'sobre.html', context)
+
+def list_projetos (request):
+    projetos = Project.objects.all().order_by('data_criacao')
+    context = {'projetos': projetos}
+    return render(request, 'projetos.html', context)
