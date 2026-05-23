46ebf10b1c49bf539d38da32ced63a27e31598cf
Vinicius Pontes Braga <viniciusbragacontatos@gmail.com>
2026-05-23 09:41:49 -0300

    chore(models): restructure models

--- portifolio/models.py
+++ portifolio/models.py
@@ -5,8 +5,8 @@ class Project (models.Model):
     descricao_curta = models.TextField()
     descricao_detalhada = models.TextField()
     thumb_projeto = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True, null=True)
-    url_git = models.CharField()
-    tecnologias = models.CharField() #utilizar separando por vígulas
+    url_git = models.URLField(blank=True, null=True)
+    tecnologias = models.CharField(max_length=255)
     data_criacao = models.DateTimeField(auto_now_add=True) #utiliza o horário do momento em que é criado
 
     def __str__(self):
@@ -58,8 +58,8 @@ class Profile (models.Model):
     bio_curta = models.TextField()
     bio_longa = models.TextField()
     email = models.EmailField()
-    link_git = models.CharField()
-    link_linkedin = models.CharField()
+    link_git = models.URLField(blank=True, null=True)
+    link_linkedin = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.nome
