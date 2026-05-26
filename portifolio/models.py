401592185fa4dd93c09087a7c8cf2a4f8aa15522
Vinicius Pontes Braga <viniciusbragacontatos@gmail.com>
2026-05-26 08:47:20 -0300

    chore(porfile-model): add profile picture

--- portifolio/models.py
+++ portifolio/models.py
@@ -54,12 +54,13 @@ class Contato (models.Model):
 
 class Profile (models.Model):
     nome = models.CharField(max_length=100)
+    foto_perfil = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
     titulo_profissional = models.CharField(max_length=255)
     bio_curta = models.TextField()
     bio_longa = models.TextField()
     email = models.EmailField()
     link_git = models.URLField(blank=True, null=True)
-    link_linkedin = models.URLField(blank=True, null=True)
+    link_linkedin = models.URLField(blank=True, null=True) 
 
     def __str__(self):
         return self.nome
